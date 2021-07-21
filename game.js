4af9ed36e5929e87a9a9f34bd212f7c39f3ef0a8
Grayman1 <54072430+Grayman1@users.noreply.github.com>
2021-07-21 19:25:26 +0000

    Initial commit

---
+++ game.js
@@ -0,0 +1,126 @@
+const {GRID_SIZE} = require('./constants');
+
+module.exports = {
+  createGameState,
+  gameLoop,
+}
+
+function initGame() {
+  const state = createGameState()
+  randomFood(state);
+  return state;
+}
+
+function createGameState() {
+  return {
+  player: {
+    pos: {
+      x: 3,
+      y: 10,
+    },
+    vel: {
+      x: 1,
+      y: 0,
+    },
+    snake: [
+      {x: 1, y: 10},
+      {x: 2, y: 10},
+      {x: 3, y: 10},
+    ],  
+  },
+  food: {
+    x: 7,
+    y: 7,
+  },
+  gridsize: GRID_SIZE,  
+  active: true,
+  };
+}
+
+function gameLoop(state) {
+  if(!state) {
+    return;
+  }
+  const playerOne = state.player;
+
+  playerOne.pos.x += playerOne.vel.x;
+  playerOne.pos.y += playerOne.vel.y;
+
+  if (playerOne.pos.x < 0 || playerOne.pos.x > GRID_SIZE || playerOne.pos.y < 0 || playerOne.pos.y > GRID_SIZE) {
+    return 2;
+
+  if (state.food.x === playerOne.pos.x && state.food.y === playerOne.pos.y) {
+    playerOne.snake.push({ ...playerOne.pos });
+    playerOne.pos.x += playerOne.vel.x;
+    playerOne.pos.y += playerOne.vel.y;
+    randomFood(state);
+  }
+
+  if (playerOne.vel.x || playerOne.vel.y) {
+    for (let cell of playerOne.snake) {
+      if (cell.x === playerOne.pos.x && cell.y === playerOne.pos.y) {
+        return 2;
+      }
+    }
+
+    playerOne.snake.push({ ...playerOne.pos });
+    playerOne.snake.shift();
+  }
+  return false;
+
+/*  const playerOne = state.players[0];
+  const playerTwo = state.players[1];
+
+  playerOne.pos.x += playerOne.vel.x;
+  playerOne.pos.y += playerOne.vel.y;
+
+  playerTwo.pos.x += playerTwo.vel.x;
+  playerTwo.pos.y += playerTwo.vel.y;
+*/
+
+}
+
+function randomfood(state) {
+  food = {
+    x: Math.floor(Math.random() * GRID_SIZE),
+    y: Math.floor(Math.random() * GRID_SIZE),
+  }
+
+  for (let cell of state.player.snake) {
+    if (cell.x === food.x && cell.y === food.y) {
+      return randomFood(state);
+    }
+  }
+/*
+  for (let cell of state.players[0].snake) {
+    if (cell.x === food.x && cell.y === food.y) {
+      return randomFood(state);
+    }
+  }
+
+  for (let cell of state.players[1].snake) {
+    if (cell.x === food.x && cell.y === food.y) {
+      return randomFood(state);
+    }
+  }
+*/
+  state.food = food;
+}
+
+function getUpdatedVelocity(keyCode) {
+  switch (keyCode) {
+    case 37: {//left
+      return: {x: -1, y: 0};
+    }
+    case 38: {//down
+      return: {x: 0, y: -1};
+    }
+    case 39: {//right
+      return: {x: 1, y: 0};
+    }
+    case 40: {//up
+      return: {x: 0, y: 1};
+    }
+
+  }
+}
